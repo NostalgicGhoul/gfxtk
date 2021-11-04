aff9ef4bb68e436581c6988877d9fe8814ed4576
nostalgicghoul <nostalgicghoul@gmail.com>
2021-11-04 14:04:04 -0700

    Updated git scripts to properly grab gfxtk's commit hash instead of the hash of whatever project is referencing gfxtk in tree

--- cmake/GenerateGitInfoHeader.cmake
+++ cmake/GenerateGitInfoHeader.cmake
@@ -1,9 +1,9 @@
 execute_process(
-        COMMAND git rev-parse HEAD
+        COMMAND git --git-dir=${CMAKE_ARGV3} rev-parse HEAD
         OUTPUT_VARIABLE OUTPUT_GIT_COMMIT_HASH
         OUTPUT_STRIP_TRAILING_WHITESPACE
 )
-FILE(WRITE ${CMAKE_ARGV3}/git_info.h
+FILE(WRITE ${CMAKE_ARGV4}/git_info.h
 "#ifndef GFXTK_GIT_COMMIT\n"
 "#define GFXTK_GIT_COMMIT \"${OUTPUT_GIT_COMMIT_HASH}\"\n"
 "#endif\n"
--- cmake/GitInfo.cmake
+++ cmake/GitInfo.cmake
@@ -1,4 +1,4 @@
 add_custom_target(gfxtk_git_info)
 add_custom_command(TARGET gfxtk_git_info
-        COMMAND ${CMAKE_COMMAND} -P ${GFXTK_SOURCE_DIR}/cmake/GenerateGitInfoHeader.cmake "${GFXTK_GIT_INFO_HEADER_PATH}"
+        COMMAND ${CMAKE_COMMAND} -P ${GFXTK_SOURCE_DIR}/cmake/GenerateGitInfoHeader.cmake "${GFXTK_SOURCE_DIR}/.git" "${GFXTK_GIT_INFO_HEADER_PATH}"
 )
